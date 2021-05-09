535d82d871d4060ab0e2540090487ebf8eb58530
Christian Javellana <cjjavellana@gmail.com>
2021-05-09 19:45:36 +0800

    Added VMWare's clarify UI Components

--- src/taskpane/app/app.module.ts
+++ src/taskpane/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClarityModule } from '@clr/angular';
 import AppComponent from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, BrowserAnimationsModule, ClarityModule],
   bootstrap: [AppComponent],
 })
 export default class AppModule {}
